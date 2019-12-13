146f7b521d4a16857d5b0fc6715f04aa38ee02c7
ikechuku <rockstarik2@gmail.com>
2019-12-13 01:38:12 +0100

    Create authentication routes

--- src/routes/main/index.js
+++ src/routes/main/index.js
@@ -2,11 +2,15 @@ import React, { Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
 
 const Home = lazy(() => import("../../containers/Home"));
+const Auth = lazy(() => import("../../containers/Auth"));
+const Password = lazy(() => import("../../containers/ForgotPassword"));
 
 const App = () => (
   <Suspense fallback={"loading"}>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/forgot-password" component={Password} />
+      <Route path="/" component={Auth} />
     </Switch>
   </Suspense>
 );
